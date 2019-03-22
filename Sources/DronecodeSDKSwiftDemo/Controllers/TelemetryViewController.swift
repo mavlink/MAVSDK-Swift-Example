1cb0de08275169da71b885aa067c82ec95b7ac7a
sbporter <sethbporter@gmail.com>
2019-03-21 22:32:43 -0600

    Add additional telemetry to TelemetryViewController

--- Sources/DronecodeSDKSwiftDemo/Controllers/TelemetryViewController.swift
+++ Sources/DronecodeSDKSwiftDemo/Controllers/TelemetryViewController.swift
@@ -8,6 +8,7 @@ private enum EntryType : Int {
     case health
     case altitude
     case latitude_longitude
+    case flight_mode
     case armed
     case groundspeed
     case battery
@@ -28,7 +29,7 @@ private class TelemetryEntry {
     }
 }
 
-class TelemetryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+class TelemetryViewController: UIViewController {
 
     @IBOutlet weak var connectionLabel: UILabel!
     @IBOutlet weak var telemetryTableView: UITableView!
@@ -56,6 +57,13 @@ class TelemetryViewController: UIViewController, UITableViewDataSource, UITableV
         entries.append(TelemetryEntry(property: "Health"))
         entries.append(TelemetryEntry(property: "Relative, absolute altitude"))
         entries.append(TelemetryEntry(property: "Latitude, longitude"))
+        entries.append(TelemetryEntry(property: "Flight Mode"))
+        entries.append(TelemetryEntry(property: "Armed"))
+        entries.append(TelemetryEntry(property: "Ground Speed"))
+        entries.append(TelemetryEntry(property: "Battery"))
+        entries.append(TelemetryEntry(property: "Attitude"))
+        entries.append(TelemetryEntry(property: "GPS"))
+        entries.append(TelemetryEntry(property: "In Air"))
 
         return entries
     }
@@ -64,8 +72,18 @@ class TelemetryViewController: UIViewController, UITableViewDataSource, UITableV
         observeConnection()
         observeHealth()
         observePosition()
+        observeFlightMode()
+        observeArmed()
+        observeGroundSpeed()
+        observeBattery()
+        observeAttitude()
+        observeGPS()
+        observeInAir()
     }
+}
 
+// MARK: - DroneCode Observations
+extension TelemetryViewController {
     private func observeConnection() {
         _ = drone.core.connectionState
                 .observeOn(MainScheduler.instance)
@@ -118,13 +136,161 @@ class TelemetryViewController: UIViewController, UITableViewDataSource, UITableV
     }
 
     private func onPositionUpdate(position: Telemetry.Position) {
-        entries[EntryType.altitude.rawValue].value = "\(position.relativeAltitudeM) m, \(position.absoluteAltitudeM) m"
+        let relativeAltString = String(format: "%.2f", position.relativeAltitudeM)
+        let absoluteAltString = String(format: "%.2f", position.absoluteAltitudeM)
+        
+        entries[EntryType.altitude.rawValue].value = "\(relativeAltString) m, \(absoluteAltString) m"
 
-        entries[EntryType.latitude_longitude.rawValue].value = "\(position.latitudeDeg) Deg, \(position.longitudeDeg) Deg"
+        let latitudeString = String(format: "%.7f", position.latitudeDeg)
+        let longitudeString = String(format: "%.7f", position.longitudeDeg)
+
+        entries[EntryType.latitude_longitude.rawValue].value = "\(latitudeString)° | \(longitudeString)°"
 
         telemetryTableView.reloadData()
     }
     
+    private func observeFlightMode() {
+        _ = drone.telemetry.flightMode
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { mode in self.onFlightModeUpdate(mode: mode)},
+                       onError: { error in print(error) })
+    }
+    
+    private func onFlightModeUpdate(mode: Telemetry.FlightMode) {
+        var modeString = ""
+        switch mode {
+        case .unknown:
+            modeString = "Unknown"
+        case .ready:
+            modeString = "Ready"
+        case .takeoff:
+            modeString = "Take Off"
+        case .hold:
+            modeString = "Hold"
+        case .mission:
+            modeString = "Mission"
+        case .returnToLaunch:
+            modeString = "RTL"
+        case .land:
+            modeString = "Land"
+        case .offboard:
+            modeString = "Offboard"
+        case .followMe:
+            modeString = "Follow Me"
+        case .UNRECOGNIZED(_):
+            modeString = "Unrecognized"
+        }
+        
+        self.entries[EntryType.flight_mode.rawValue].value = modeString
+        self.telemetryTableView.reloadData()
+    }
+    
+    private func observeArmed() {
+        _ = drone.telemetry.armed
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { armed in self.onArmedUpdate(armed: armed) },
+                       onError: { error in print(error) })
+    }
+    
+    private func onArmedUpdate(armed: Bool) {
+        self.entries[EntryType.armed.rawValue].value = "\(armed)"
+        self.telemetryTableView.reloadData()
+    }
+    
+    private func observeGroundSpeed() {
+        _ = drone.telemetry.groundSpeedNed
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { groundSpeed in self.onGroundSpeedUpdate(groundSpeed: groundSpeed) },
+                       onError: { error in print(error) })
+    }
+    
+    private func onGroundSpeedUpdate(groundSpeed: Telemetry.SpeedNed) {
+        let northFormattedString = String(format: "%.3f", groundSpeed.velocityNorthMS)
+        let eastFormattedString = String(format: "%.3f", groundSpeed.velocityEastMS)
+        let downFormattedString = String(format: "%.3f", groundSpeed.velocityDownMS)
+
+        self.entries[EntryType.groundspeed.rawValue].value = "N: \(northFormattedString) m/s | E: \(eastFormattedString) m/s | D: \(downFormattedString) m/s"
+        self.telemetryTableView.reloadData()
+    }
+    
+    private func observeBattery() {
+        _ = drone.telemetry.battery
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { battery in self.onBatteryUpdate(battery: battery) },
+                       onError: { error in print(error) })
+    }
+    
+    private func onBatteryUpdate(battery: Telemetry.Battery) {
+        let voltageString = String(format: "%.2f", battery.voltageV)
+
+        self.entries[EntryType.battery.rawValue].value = "\(battery.remainingPercent * 100.0) % | \(voltageString) V"
+        self.telemetryTableView.reloadData()
+    }
+    
+    private func observeAttitude() {
+        _ = drone.telemetry.attitudeEuler
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { attitude in self.onAttitudeUpdate(attitude: attitude) },
+                       onError: { error in print(error) })
+    }
+    
+    private func onAttitudeUpdate(attitude: Telemetry.EulerAngle) {
+        let rollString = String(format: "%.2f", attitude.rollDeg)
+        let pitchString = String(format: "%.2f", attitude.pitchDeg)
+        let yawString = String(format: "%.2f", attitude.yawDeg)
+
+        self.entries[EntryType.attitude.rawValue].value = "Roll: \(rollString) | Pitch: \(pitchString) | Yaw: \(yawString)"
+        self.telemetryTableView.reloadData()
+    }
+    
+    private func observeGPS() {
+        _ = drone.telemetry.gpsInfo
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext:{ gps in self.onGPSUpdate(gps: gps) },
+                       onError: { error in print(error) })
+    }
+    
+    private func onGPSUpdate(gps: Telemetry.GpsInfo) {
+        var fixTypeString = ""
+        
+        switch gps.fixType {
+        case .noGps:
+            fixTypeString = "GPS"
+        case .noFix:
+            fixTypeString = "Fix"
+        case .fix2D:
+            fixTypeString = "2D"
+        case .fix3D:
+            fixTypeString = "3d"
+        case .fixDgps:
+            fixTypeString = "Dgps"
+        case .rtkFloat:
+            fixTypeString = "RTK Float"
+        case .rtkFixed:
+            fixTypeString = "RTK Fixed"
+        case .UNRECOGNIZED(_):
+            fixTypeString = "Unrecognized"
+        }
+        
+        self.entries[EntryType.gps.rawValue].value = "Satellites: \(gps.numSatellites) | Fix: \(fixTypeString)"
+        self.telemetryTableView.reloadData()
+    }
+    
+    private func observeInAir() {
+        _ = drone.telemetry.inAir
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext:{ inAir in self.onInAirUpdate(inAir: inAir) },
+                       onError: { error in print(error) })
+    }
+    
+    private func onInAirUpdate(inAir: Bool) {
+        self.entries[EntryType.in_air.rawValue].value = "\(inAir)"
+        self.telemetryTableView.reloadData()
+    }
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+extension TelemetryViewController: UITableViewDataSource, UITableViewDelegate  {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
